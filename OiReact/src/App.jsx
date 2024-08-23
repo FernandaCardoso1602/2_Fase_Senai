95eea4dc82057609627ab60031cba9948a184df6
FernandaCardoso1602 <fernanda_marciano@estudante.sesisenai.org.br>
2024-08-23 17:16:14 -0300

    Criação do primeiro projeto com React bonitão

---
+++ OiReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1>Dinossauros</h1>
+        <h2>Carnívoros</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est reiciendis nihil, veritatis porro, aspernatur error ducimus blanditiis suscipit ad tempora quae. Ratione ab atque architecto repudiandae perspiciatis sapiente aliquam consequuntur.</p>
+    </>
+  )
+}
+
+export default App
