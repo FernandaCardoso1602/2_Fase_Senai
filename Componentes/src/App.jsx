f88ace75fb5a5e0df7d6a880c8297d2ee053cb28
FernandaCardoso1602 <fernanda_marciano@estudante.sesisenai.org.br>
2024-08-29 17:20:53 -0300

    Add criação de componentes e mais umas coisas legais e gostosinhas

---
+++ Componentes/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Atalhos do VS Code</h1>
+    <h2>Coisas legais para facilitar tua vida</h2>
+    <p>
+      Comando para criar o emmet da ul: ul▶li*6 
+    </p>
+    <ul>
+      <li>Atalhos: Alt + z para quebra automática de linha</li>
+      <li>Atalhos: Alt + clique esquerdo para adicionar novos cursores na tela</li>
+      <li>Atalhos: Ctrl + shift + k para excluir uma linha inteira</li>
+      <li>Atalhos: Ctrl + ; para comentar/descomentar</li>
+      <li>Atalhos: Windows + . abre teclado de emojis 😮</li>
+      <li>Atalhos: Alt + ↕ move uma linha atual ou o bloco </li>
+      <li>Atalhos: Alt + shift + ↕ copia a linha atual ou o bloco </li>
+      <li>Atalhos: Ctrl + j ou ctrl + ' abre/fecha o terminal </li>
+    </ul>
+    <p>Para esfregar varios cursores pela tela (digitar varias coisas ao mesmo tempo): Clica com a rodinha e esfrega ela pela tela</p>
+    <p>Extensões:</p>
+    <ul>
+      <li>Extensões: ES7 React/Redux/GraphQL/React-Native snippets</li>
+      <li>Extensões: Auto Rename Tag</li>
+      <li>Extensões: Color Highlight</li>
+      <li>Extensões: </li>
+      <li>Extensões: </li>
+    </ul>
+    </>
+  )
+}
+
+export default App
