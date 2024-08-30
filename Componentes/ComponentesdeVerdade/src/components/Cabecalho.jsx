665fb8f958e5f4a8442d0c95535d21d7b29a34dc
FernandaCardoso1602 <fernanda_marciano@estudante.sesisenai.org.br>
2024-08-30 17:06:21 -0300

    Hoje adicionamos cores e imagens ao projeto

--- Componentes/ComponentesdeVerdade/src/components/Cabecalho.jsx
+++ Componentes/ComponentesdeVerdade/src/components/Cabecalho.jsx
@@ -2,7 +2,7 @@ import './Cabecalho.css'
 function Cabecalho() {
   return (
     <div className="Cabecalho-container">
-      <h1>Treinar Dragão</h1>
+      <h1>House of treinar seu Dragão</h1>
     </div>
   )
 }
